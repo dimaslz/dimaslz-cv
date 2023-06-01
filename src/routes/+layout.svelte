010cd654bc1fef97f1207d428a5b908f0c113bef
dimaslz <me@dimaslz.io>
2023-06-01 14:29:27 +0200

    chore: move network links to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,9 +21,17 @@
 	</main>
 
 	{#if !isPDFVersion}
-		<footer>
+		<!-- footer -->
+		<footer class="mb-12">
 			<p class="text-sm p-4 text-gray-600">
 				built with ♥️ from Barcelona with <a href="https://kit.svelte.dev/" target="_blank" class="text-orange-600 hover:text-orange-800" rel="noreferrer">svelte</a> | <a href="https://twitter.com/dimaslz" class="text-blue-600 hover:text-blue-800" target="_blank" rel="noreferrer">@dimaslz</a></p>
 		</footer>
+
+		<!-- network -->
+		<div class="fixed p-4 bottom-0 right-0 flex">
+			<a href="https://github.com/dimaslz" class="p-2 text-sm hover:opacity-60 hover:bg-slate-100">gh</a>
+			<a href="https://www.linkedin.com/in/dimaslopezzurita" class="p-2 text-sm hover:opacity-60 hover:bg-slate-100">in</a>
+			<a href="https://twitter.com/dimaslz" class="p-2 text-sm hover:opacity-60 hover:bg-slate-100">tw</a>
+		</div>
 	{/if}
 </div>
